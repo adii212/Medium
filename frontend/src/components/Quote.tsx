64be4d5f411be2af3f9eb5a1ddd5fd6b24b40fd4
adii212 <49607184+adii212@users.noreply.github.com>
2024-06-05 00:20:58 +0530

    initial frontend checkin

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,10 @@
+
+export const Quote = () => {
+    return <div className="bg-slate h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+            <div className="max-w-md text-center text-3xl text-bold">
+                Carpe Diem! Seize the Day
+            </div>
+        </div>
+    </div>
+}
