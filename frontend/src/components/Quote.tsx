5879fa10c33d8da3bc0cec2f45f6e049120c350b
adii212 <49607184+adii212@users.noreply.github.com>
2024-06-05 00:57:47 +0530

    coding signup and auth component

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,9 +1,17 @@
 
 export const Quote = () => {
-    return <div className="bg-slate h-screen flex justify-center flex-col">
+    return <div className="bg-slate-100 h-screen flex justify-center flex-col">
         <div className="flex justify-center">
-            <div className="max-w-md text-center text-3xl text-bold">
-                Carpe Diem! Seize the Day
+            <div className="max-w-lg">
+                <div className="text-3xl text-bold">
+                    Carpe Diem! Seize the Day
+                </div>
+                <div className="max-wd-md text-xl font-semibold text-left mt-2">
+                    Aditya Pandey
+                </div>
+                <div className="max-w-md text-sm font-light text-slate-400">
+                    CEO | Aditya Technologies ltd
+                </div>
             </div>
         </div>
     </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,15 @@
 import { Quote } from "../components/Quote"
+import { Auth } from "../components/Auth"
 
 export const Signup = () => {
     return <div>
-        <Quote />
-        Signup
+        <div className="grid grid-cols-2">
+            <div>
+                <Auth type={"signup"} />
+            </div>
+            <div className="invisible lg:visible">
+                <Quote />
+            </div>
+        </div>
     </div>
 }
